2960c3cbd92bab336bcf795d120cd88a72c17f4b
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-06-11 19:05:05 -0500

    Revert "Revert "dont infinite loop""
    This reverts commit 22430e355b17e7e3a8887daf44d6ac3e67ad1b45.
    
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/parrotgeek/parrotmodfloapp/MyService.java
+++ app/src/main/java/com/parrotgeek/parrotmodfloapp/MyService.java
@@ -138,7 +138,7 @@ public class MyService extends Service {
         } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "EXEC ERROR: " + e.getLocalizedMessage(), Toast.LENGTH_LONG).show();
             setRunning(false);
-            return "";
+            return null;
         }
     }
 
@@ -168,8 +168,13 @@ public class MyService extends Service {
         final String[] cmd = new String[]{"su", "-c", " sh '" + script + "' </dev/null >/dev/null 2>&1"};
         new Thread(new Runnable() {
             public void run() {
+                String str = null;
                 while (true) {
-                    execCmd(cmd);
+                    str = execCmd(cmd);
+                    if(str == null) {
+                        Log.e(TAG, "STOP LOOP due to exec error");
+                        return;
+                    }
                     Log.e(TAG, "run: for some reason script stopped, restarting");
                 }
             }
