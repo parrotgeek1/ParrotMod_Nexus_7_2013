463c9f332a731e979963a59641f16e7a210752fc
Ethan Nelson-Moore <parrotgeek1@gmail.com>
2016-08-02 21:57:52 -0500

    gobbler

--- app/src/main/java/com/parrotgeek/parrotmodfloapp/SuShell.java
+++ app/src/main/java/com/parrotgeek/parrotmodfloapp/SuShell.java
@@ -2,9 +2,39 @@ package com.parrotgeek.parrotmodfloapp;
 
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class SuShell {
+    class StreamGobbler extends Thread
+    {
+        InputStream is;
+        String type;
+
+        StreamGobbler(InputStream is, String type)
+        {
+            this.is = is;
+            this.type = type;
+        }
+
+        public void run()
+        {
+            try
+            {
+                InputStreamReader isr = new InputStreamReader(is);
+                BufferedReader br = new BufferedReader(isr);
+                String line=null;
+                while ( (line = br.readLine()) != null)
+                    System.out.println(type + ">" + line);
+            } catch (IOException ioe)
+            {
+                ioe.printStackTrace();
+            }
+        }
+    }
     private java.lang.Process proc;
     private DataOutputStream dos;
 
@@ -12,6 +42,17 @@ public class SuShell {
         try {
             proc = Runtime.getRuntime().exec("su");
             dos = new DataOutputStream(proc.getOutputStream());
+            // any error message?
+            StreamGobbler errorGobbler = new
+                    StreamGobbler(proc.getErrorStream(), "ERROR");
+
+            // any output?
+            StreamGobbler outputGobbler = new
+                    StreamGobbler(proc.getInputStream(), "OUTPUT");
+
+            // kick them off
+            errorGobbler.start();
+            outputGobbler.start();
         } catch (Exception e) {
             Log.e("sushell",e.getLocalizedMessage());
             Crasher.crash();
