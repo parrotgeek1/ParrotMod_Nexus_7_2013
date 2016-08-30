c0e4107833196742fc5c73bc87b971a888a23ad4
Ethan Nelson-Moore <ethan@Ethans-MacBook-Pro.local>
2016-08-30 15:38:16 -0500

    lolwut

--- app/src/main/java/com/parrotgeek/parrotmodfloapp/SuShell.java
+++ app/src/main/java/com/parrotgeek/parrotmodfloapp/SuShell.java
@@ -4,7 +4,6 @@ import android.util.Log;
 
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
-import java.io.Externalizable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
